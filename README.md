6146b7a744b828f650ea3b78b5ff936f84bf8afa
hale <hale.huo@zhangmen.com>
2019-04-07 17:59:28 +0800

    up

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # graphql
 graphql demo
+
+# 安装依赖
+
+> npm i
+
+> npm install -g nodemon
+
+或
+> npm install --save-dev nodemon
+
+# 启动服务
+
+> npm start
---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+	type Query {
+		hello: String,
+		world: String,
+		yes: String,
+		no(name:String): String
+	}
+`);
+
+var root = {
+	hello: () => 'Hello world!',
+	world: () => 'hi world',
+	yes: () => 'yes!',
+	no: (name) => {
+		console.log(name);
+		
+		return 'no:' + name.name;
+	}
+};
+
+var app = express();
+app.use('/', graphqlHTTP({
+	schema: schema,
+	rootValue: root,
+	graphiql: true,
+}));
+
+app.listen(3000, () => console.log('Now browse to localhost:3000/graphql'));
