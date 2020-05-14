71110ebc4de8d2eab0740872f2ff8325aa551f01
hale.huo <hale.huo@zhangmen.com>
2020-05-14 21:55:06 +0800

    修改操作

--- README.md
+++ README.md
@@ -13,3 +13,27 @@ graphql demo
 # 启动服务
 
 > npm start
+
+# 操作
+浏览器中访问：http://localhost:3000/会打开图形界面
+
+## 查询
+```
+{
+  hello
+}
+或
+query {
+  hello
+}
+```
+## 修改
+```
+mutation {
+  setMessage(message:"hello")
+}
+加别名
+mutation {
+  update: setMessage(message:"hello")
+}
+```
--- src/server.js
+++ src/server.js
@@ -9,15 +9,25 @@ var schema = buildSchema(`
 		yes: String,
 		no(name:String): String
 	}
+	type Mutation {
+		setMessage(message: String): String,
+	}
 `);
 
 var root = {
 	hello: () => 'Hello world!',
 	world: () => 'hi world',
 	yes: () => 'yes!',
+	//传参查询
 	no: (name) => {
 		console.log(name);
 		return name.name;
+	},
+	// 修改操作
+	setMessage: function ({ message }) {
+		console.log("修改参数：", message);
+
+		return message;
 	}
 };
 
